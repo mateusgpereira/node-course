df6e39b47d1801fe3dc69e5b8b6ede51859ba877
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2019-03-07 18:44:24 -0300

    setting crud commands

---
+++ app.js
@@ -0,0 +1,52 @@
+const yargs = require('yargs')
+const chalk = require('chalk')
+
+const getNotes = require('./notes.js')
+
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log(`Title: ${argv.title}\n Body: ${argv.body}`)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: "List all notes",
+    handler: function(){
+        console.log('All notes ...')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse()
