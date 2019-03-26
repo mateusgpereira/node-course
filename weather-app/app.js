6e1ec621b0b95887618aaa4df23beb169a8d4932
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2019-03-26 18:01:17 -0300

    Add a new request for mapbox api to get lat and long

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,3 +5,10 @@ const url = 'https://api.darksky.net/forecast/ec3dfd2bfc8eaf8ecc8e5145942848ad/3
 request({url, json: true}, (err, res) => {
     console.log(`${res.body.daily.data[0].summary} It is currently ${res.body.currently.temperature} degrees out. There is a ${res.body.currently.precipProbability}% chance of rain.`)
 })
+
+
+const urlMap = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWF0ZXVzLXBlcmVpcmEiLCJhIjoiY2pxbXVpZ29jMDFraDN4cGJnNWZ4anRpcyJ9.1OVBwXPLTNemyRqS8eT8lg&cachebuster=1553632987678&autocomplete=true&limit=1'
+
+request({url: urlMap, json: true}, (err, res) => {
+    console.log(`Longitude: ${res.body.features[0].center[0]} \nLatitude: ${res.body.features[0].center[1]}`)
+})
