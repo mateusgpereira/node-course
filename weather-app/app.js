cf9e3b48a4ec67cce98d9144872db4df18d8b52b
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2019-05-13 20:01:54 -0300

    separated geocode function in a module

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = 'https://api.darksky.net/forecast/ec3dfd2bfc8eaf8ecc8e5145942848ad/37.8267,-122.4233?lang=pt&units=si'
 
@@ -9,18 +10,41 @@ const request = require('request')
 
 
 
-const urlMap = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWF0ZXVzLXBlcmVpcmEiLCJhIjoiY2pxbXVpZ29jMDFraDN4cGJnNWZ4anRpcyJ9.1OVBwXPLTNemyRqS8eT8lg&cachebuster=1553632987678&autocomplete=true&limit=1'
+// const urlMap = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWF0ZXVzLXBlcmVpcmEiLCJhIjoiY2pxbXVpZ29jMDFraDN4cGJnNWZ4anRpcyJ9.1OVBwXPLTNemyRqS8eT8lg&cachebuster=1553632987678&autocomplete=true&limit=1'
 
-request({url: urlMap, json: true}, (err, res) => {
-    if(err) {
-        console.log('Unable to connect!')
+// request({url: urlMap, json: true}, (err, res) => {
+//     if(err) {
+//         console.log('Unable to connect!')
 
-    } else if(res.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.')
+//     } else if(res.body.features.length === 0) {
+//         console.log('Unable to find location. Try another search.')
 
-    } else {
-        console.log(`Longitude: ${res.body.features[0].center[0]} \nLatitude: ${res.body.features[0].center[1]}`)
+//     } else {
+//         console.log(`Longitude: ${res.body.features[0].center[0]} \nLatitude: ${res.body.features[0].center[1]}`)
 
-    }
+//     }
     
+// })
+
+// const geocode = (address, callback) => {
+//     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoibWF0ZXVzLXBlcmVpcmEiLCJhIjoiY2pxbXVpZ29jMDFraDN4cGJnNWZ4anRpcyJ9.1OVBwXPLTNemyRqS8eT8lg&cachebuster=1553632987678&autocomplete=true&limit=1`
+
+//     request({ url , json: true}, (error, res) => {
+//         if(error) {
+//             callback('Unable to connect to location services!', undefined)
+//         } else if(res.body.features.length === 0) {
+//             callback('Unable to find location. Try another search.', undefined)
+//         } else {
+//             callback(undefined,{
+//                 lat: res.body.features[0].center[0],
+//                 long: res.body.features[0].center[1],
+//                 location: res.body.features[0].place_name
+//             })
+//         }
+//     })
+// }
+
+geocode('Porto Alegre', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
